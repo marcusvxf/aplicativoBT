3c137c3c6c112d537ebd760b3585676cfef36e87
Diogodhs <dhs@cin.ufpe.br>
2020-04-01 22:28:21 -0300

    Construção da tela iniciado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonList, IonItemDivider, IonItem, IonLabel, IonCheckbox } from '@ionic/react';
+import React, { useState } from 'react';
 import './Home.css';
 
+const checkboxList = [
+  { val: 'Ativar notificações', isChecked: true },
+  { val: 'Vibrar ao receber notificação', isChecked: false },
+];
+
 const Home: React.FC = () => {
+
+  const [checked, setChecked] = useState(false);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Teste Git</IonTitle>
+          <IonTitle>Configurações</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent>
+
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Teste Git</IonTitle>
+            <IonTitle size="large">Configurações</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+
+        <h2>Conta</h2>
+        <p>E-mail: abc@biotechconsultoria.com</p>
+        <IonButton expand="block" fill="outline">Logout</IonButton>
+
+        <h2>Notificações</h2>
+        <IonList>
+            {checkboxList.map(({ val, isChecked }, i) => (
+              <IonItem key={i}>
+                <IonLabel>{val}</IonLabel>
+                <IonCheckbox slot="end" value={val} checked={isChecked} />
+              </IonItem>
+            ))}
+          </IonList>
+
       </IonContent>
     </IonPage>
   );
