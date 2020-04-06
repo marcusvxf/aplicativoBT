df5d06fefe76ff08dedb57c369ee3f570b8ddccf
marcusvxf <marcusviniciusleite1@hotmail.com>
2020-04-06 18:56:53 -0300

    Atualização do desing adicionando cards na tela

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,7 +1,7 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle,IonIcon, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle,IonIcon, IonToolbar, IonCard, IonCardHeader, IonCardContent, IonItem } from '@ionic/react';
 import React from 'react';
 import './Page.css';
-import { settingsOutline, optionsOutline } from 'ionicons/icons';
+import { optionsOutline,checkbox } from 'ionicons/icons';
 
 const Tab1: React.FC = () => {
   return (
@@ -10,13 +10,39 @@ const Tab1: React.FC = () => {
         
         <IonToolbar class="new-background-color">
           <IonButtons slot="end"><IonMenuButton><IonIcon size="large" icon={optionsOutline}></IonIcon></IonMenuButton></IonButtons>
-          <IonTitle>Bem vindo "nome da pessoa"</IonTitle>
+          <IonTitle color="dark"><h2>Bem vindo</h2></IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent class="principal">
-        <IonContent class="login">
+        <IonCard>
+          <IonCardHeader>        
+            <h1>Horarios de plantões</h1>
 
-        </IonContent>
+          </IonCardHeader>
+          <IonCardContent>              
+    
+              <h1>Seg - 13h as 15h</h1>
+              <h1>Qua - 15h as 17h</h1>
+
+          </IonCardContent>
+
+        </IonCard>
+        <IonCard>
+          <IonCardHeader>
+            <h1>Atividades</h1>
+          </IonCardHeader>
+          <IonCardContent>
+
+               <h1>Atualizar planilhas</h1>                 
+               <h1>RG</h1>
+     
+
+          </IonCardContent>
+        </IonCard>
+       {/* <IonContent class="login" >
+
+
+       </IonContent>*/}
         
       </IonContent >
     </IonPage>
