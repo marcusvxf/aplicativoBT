33294d55f3584ba36537c4ae2cb95d8da49c1e16
Diogodhs <dhs@cin.ufpe.br>
2020-04-06 22:36:25 -0300

    adicionando o botão para modificar perfil e uma imagem para o perfil

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonList, IonItemDivider, IonItem, IonLabel, IonCheckbox } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonList, IonItemDivider, IonItem, IonLabel, IonCheckbox, IonThumbnail, IonImg } from '@ionic/react';
 import React, { useState } from 'react';
 import './Home.css';
 
@@ -6,6 +6,12 @@ const checkboxList = [
   { val: 'Ativar notificações', isChecked: true },
   { val: 'Vibrar ao receber notificação', isChecked: false },
 ];
+
+type Item = {
+  src: string;
+  text: string;
+};
+const items: Item[] = [{ src: 'http://placekitten.com/g/200/300', text: 'Nome de perfil acompanhado com figura' }];
 
 const Home: React.FC = () => {
 
@@ -29,6 +35,17 @@ const Home: React.FC = () => {
 
         <h2>Conta</h2>
         <p>E-mail: abc@biotechconsultoria.com</p>
+        <IonList>
+          {items.map((image, i) => (
+            <IonItem key={i}>
+              <IonThumbnail slot="start">
+                <IonImg src={image.src} />
+              </IonThumbnail>
+              <IonLabel>{image.text}</IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
+        <IonButton expand="block" fill="outline">Editar perfil</IonButton>
         <IonButton expand="block" fill="outline">Logout</IonButton>
 
         <h2>Notificações</h2>
