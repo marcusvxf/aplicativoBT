a55b1d1ffbeef7f45c00a91c9d142a1fc82dace4
unknown <marcusviniciusleite1@hotmail.com>
2020-06-22 11:22:00 -0300

    mudando layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Menu from './components/Menu';
-import Tab1 from './pages/tab1';
+import Tab1 from './pages/Tab1';
 import React, { useState } from 'react';
 import { IonApp, IonRouterOutlet, IonSplitPane, IonTabs, IonTabBar, IonButton, IonTabButton, IonIcon } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
-import Tab2 from './pages/tab2';
-import Tab3 from './pages/tab3';
+import Tab2 from './pages/Tab2';
+import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -40,14 +40,14 @@ const App: React.FC = () => {
 
           <IonTabs>
             <IonRouterOutlet id="main">          
-              <Route path="/tab1" component={Tab1} exact={true}/>
-              <Route path="/tab2" component={Tab2} exact={true}/>
-              <Route path="/tab3" component={Tab3} exact={true}/>
+              <Route path="/Tab1" component={Tab1} exact={true}/>
+              <Route path="/Tab2" component={Tab2} exact={true}/>
+              <Route path="/Tab3" component={Tab3} exact={true}/>
               <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
 
             </IonRouterOutlet>
             <IonTabBar slot="bottom">
-              <IonTabButton tab="tab1" href="/tab1">
+              <IonTabButton tab="Tab1" href="/tab1">
                 <IonIcon size="large" icon={homeOutline} />
               </IonTabButton>
 
