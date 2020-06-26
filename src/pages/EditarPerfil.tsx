51916916388e990f24d4fe8f03cd8cce31186c5d
Sara Nicoly <saranicoly0@gmail.com>
2020-06-26 13:04:19 -0300

    tela de perfil

--- src/pages/EditarPerfil.tsx
+++ src/pages/EditarPerfil.tsx
@@ -1,73 +1,44 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonList, IonItem, IonLabel, IonCheckbox, IonThumbnail, IonImg, IonInput, IonItemDivider } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonInput, IonBackButton, IonIcon, IonButtons,IonButton, IonItem, IonLabel, IonCheckbox, IonFab, IonFabButton } from '@ionic/react';
 import React, { useState } from 'react';
+import {arrowBackOutline, star} from "ionicons/icons" ;
 import './EditarPerfil.css';
 
-const checkboxList = [
-  { val: 'Ativar notificações', isChecked: true },
-  { val: 'Vibrar ao receber notificação', isChecked: false },
-];
-
-type Item = {
-  src: string;
-  text: string;
-};
-const items: Item[] = [{ src: 'http://placekitten.com/g/200/300', text: 'Nome de perfil acompanhado com figura' }];
-
 const Home: React.FC = () => {
-
-  const [checked, setChecked] = useState(false);
   const [text, setText] = useState<string>();
   const [number, setNumber] = useState<number>();
-
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar class="new-background-color">
-          <IonTitle color="dark"><h2>Editar Perfil</h2></IonTitle>
+        <IonToolbar class="toolbar">
+        <IonButtons slot="start"><IonBackButton defaultHref="tab3" color="dark" text="" icon={arrowBackOutline}> </IonBackButton></IonButtons>
+          <IonTitle color="dark"><h3>Perfil</h3></IonTitle>
         </IonToolbar>
       </IonHeader>
 
-      <IonContent class="principal">
-
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Editar Perfil</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-
-        <h2>Conta</h2>
-        <p>E-mail: abc@biotechconsultoria.com</p>
-
-          {items.map((image, i) => (
-            <IonItem key={i} class="principal">
-              <IonThumbnail slot="start">
-                <IonImg src={image.src}/>
-              </IonThumbnail>
-              <IonLabel>{image.text}</IonLabel>
-            </IonItem>
-          ))}
-
-        <IonButton expand="block" fill="solid" href="/Home">Home</IonButton>
-
-        <h2>Editando o perfil</h2>
-          <IonItem class="principal">
-            <IonLabel position="floating">Novo nome de perfil</IonLabel>
+      <IonContent>
+        <div className="perfil">
+          <img className="imagemPerfil" src="https://image.freepik.com/vetores-gratis/homem-negocios-perfil-caricatura_18591-58479.jpg"/>
+          <div className="edit-text">Editar</div>
+        </div>
+        <div className="alinhamento-conteudo">
+          <IonItem>
+            <IonLabel position="floating">Nome</IonLabel>
             <IonInput value={text}></IonInput>
           </IonItem>
-
-          <IonItem class="principal">
-            <IonLabel position="floating">Nova senha</IonLabel>
-            <IonInput value={text}></IonInput>
+          <IonItem>
+            <IonLabel position="floating">Senha</IonLabel>
+            <IonInput type="password" value={text}></IonInput>
           </IonItem>
-
-          <IonItem class="principal">
-            <IonLabel position="floating">Digite a nova senha novamente</IonLabel>
-            <IonInput value={text}></IonInput>
+          <IonItem>
+            <IonLabel position="floating">Confirmar senha</IonLabel>
+            <IonInput type="password" value={text}></IonInput>
           </IonItem>
-        <IonButton expand="block" fill="solid">Confirmar edição</IonButton>
-
-      </IonContent>
-    </IonPage>
+        </div>
+        <div className="save-bt">
+          <IonButton className="btnSave" href="tab3" fill="outline">Salvar</IonButton>
+        </div>
+        </IonContent>
+        </IonPage>
   );
 };
 
