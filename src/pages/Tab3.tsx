445d8536cd6223aeb2ffa358fb8eaef00991ffac
mavitest <soaresmunizm@gmail.com>
2020-07-04 23:04:12 -0300

    aba de configuração e report

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,6 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonButtons,IonMenuButton,IonIcon, IonItem, IonLabel, IonCheckbox, IonFab, IonFabButton } from '@ionic/react';
+import { IonImg, IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonButtons,IonMenuButton,IonIcon, IonItem, IonLabel, IonCheckbox, IonFab, IonFabButton, IonButton } from '@ionic/react';
 import React, { useState } from 'react';
-import {optionsOutline, add} from "ionicons/icons" ;
+import {menuOutline, close, cog} from "ionicons/icons" ;
 import "./check.css";
 
 const checkboxList = [
@@ -20,37 +20,51 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar class="toolbar">
-          <IonButtons slot="end"><IonMenuButton><IonIcon size="large" icon={optionsOutline}></IonIcon></IonMenuButton></IonButtons>
+          <IonButtons slot="end"><IonMenuButton><IonIcon size="large" icon={menuOutline}></IonIcon></IonMenuButton></IonButtons>
+            <div className="container">
 
+              <div className="container-logo">
+
+                <div>
+
+                  < IonImg className="img" src="https://i.ibb.co/qgTwBTd/Nova-logo-laranja-e-azul.png"></IonImg>
+
+                </div> 
+
+                  <div className="name-logo">
+
+                    <p className="biotech-name"><strong>Bio</strong><span>tech</span> </p>
+                    <p className="biotech-name-consultoria">CONSULTORIA</p>
+
+                  </div>
+
+               </div>
+
+            </div>
         </IonToolbar>
       </IonHeader>
+
       <IonContent>
         <div className= "check-title">
 
-          <h1 className="report-title">Report</h1>
-          <h5 className="date-title">22/06</h5>
+          <p className="report-title">Report</p>
+          <p className="date-title">22/06</p>
 
         </div>
+
         {checkboxList.map(({ val, isChecked }, i) => (
           <IonItem key={i}>
 
-            <IonLabel className="label" >{val}</IonLabel>
+            <IonLabel color="secondary-contrast"  className="label" ><p className="label-text">{val}</p></IonLabel>
 
             <IonCheckbox className="checkbox" slot="start" value={val} checked={isChecked} />
-
+              <IonIcon className="icon-close" icon={close} />
           </IonItem  >
 
         ))}
-        <div className="new-list">
-
-          <IonIcon  icon={add} />
-          <p className="new-list-name">  Nova tarefa</p>
        
-        </div>
-      
-
-
-
+         <IonButton className="button-add" fill="clear"><span>+</span><p>Nova tarefa</p></IonButton> 
+          
       </IonContent>
     </IonPage>
   );
